fd25e06155bdacae0d8503e29efd23554dd379bc
Byeonggon Lee <gonny952@gmail.com>
2016-05-29 21:59:23 +0900

    Commit

--- hanoi.c
+++ hanoi.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void hanoi(int n, int a, int b, int c)
+void hanoi(int n, int a, int b, int c, int* count)
 {
     if(n > 0)
     {
-        hanoi(n - 1, a, c, b);
+        hanoi(n - 1, a, c, b, count);
         printf("%d -> %d\n", a, c);
-        hanoi(n - 1, b, a, c);
+        ++(*count);
+        hanoi(n - 1, b, a, c, count);
     }
 }
 
@@ -17,7 +18,9 @@ int main()
     while(true)
     {
         scanf("%d", &n);
-        hanoi(n, 1, 2, 3);
+        int count = 0;
+        hanoi(n, 1, 2, 3, &count);
+        printf("count: %d\n", count);
         printf("\n");
     }
     return 0;
